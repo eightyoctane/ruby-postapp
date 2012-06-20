f3bab54bd8234c90155a727c4d1eca635f7e6e4b
Gary Gross <eightyoctane@yahoo.com>
2012-06-19 17:03:24 -0700

    final heroku working version for github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postappv3::Application.config.secret_token = 'a8700dcbd32ad2232c2788570765610f7d00a1fcd99ba2812680165ebb00a0abfefb9344416133d24297d459c9f0f8839b266453fb32402067eeae53470a1ea5'
